531e07b76e2bae867c048f614fd81f4e78ba3e25
realvesd <43302742+realvesd@users.noreply.github.com>
2019-03-14 17:08:34 -0300

    Create questao1menu.c

---
+++ questao1menu.c
@@ -0,0 +1,40 @@
+printf("!!!MENU!!!\n\n1 - 9: Reexibir Menu;\n\n0: Terminar Execução;\n\n\nEscolha uma opção no menu: ");
+}
+
+int main(void) {
+
+  int opcao, loop =1;
+Menu();
+scanf("%i", &opcao);
+
+while(loop ==1) {
+    switch (opcao) {
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+        Menu();
+        scanf("%d", &opcao);
+        break;
+      case 0:
+      printf("Programa encerrado!\n");
+      loop = 0;
+        break;
+      default:
+        printf("Opção desejada é inexistente, tente novamente: ");
+        Menu();
+        scanf("%i", &opcao);
+        break;
+
+
+
+    }
+}
+
+  return 0;
+}
